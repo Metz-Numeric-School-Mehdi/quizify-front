d76bc8bc1aa15c23a566626a94de681e4842ac4c
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-03-01 22:32:36 +0100

    ✨ feat: create home page

---
+++ pages/home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Bienvenue sur quizify</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import isAuthenticated from '~/middlewares/isAuthenticated';
+
+</script>
