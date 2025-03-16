3582fdc87528924b2ec35f2e2f2e5273f3848ef0
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-03-16 20:40:32 +0100

    🚧 work in progress

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -1,76 +1,52 @@
 <template>
-    <Sidebar>
-        <SidebarContent>
-            <SidebarGroup>
-                <SidebarGroupLabel>Application</SidebarGroupLabel>
-                <SidebarGroupContent>
-                    <SidebarMenu>
-                        <SidebarMenuItem v-for="item in items" :key="item.title">
-                            <SidebarMenuButton asChild>
-                                <a :href="item.url">
-                                    <component :is="item.icon" />
-                                    <span>{{ item.title }}</span>
-                                </a>
-                            </SidebarMenuButton>
-                        </SidebarMenuItem>
-                    </SidebarMenu>
-                </SidebarGroupContent>
-            </SidebarGroup>
-        </SidebarContent>
-        <SidebarFooter>
-              <NavUser :user="data.user" />
-       </SidebarFooter>
-    </Sidebar>
+  <Sidebar>
+    <SidebarContent>
+      <SidebarGroup>
+        <SidebarGroupLabel>Application</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            <SidebarMenuItem v-for="item in items" :key="item.title">
+              <SidebarMenuButton asChild>
+                <ul>
+                  <li v-for="(items, index) in sidebarItems" :key="index" :to="items.path">
+                    <RouterLink>
+                      <Icon :name="items.icon" />
+                      {{ items.name }}
+                    </RouterLink>
+                  </li>
+                </ul>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </SidebarContent>
+    <SidebarFooter>
+      <NavUser :user="data.user" />
+    </SidebarFooter>
+  </Sidebar>
 </template>
 
 <script setup lang="ts">
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-vue-next"
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-vue-next";
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarGroup,
-    SidebarGroupContent,
-    SidebarGroupLabel,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-    SidebarFooter,
-} from "@/components/ui/sidebar"
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarFooter,
+} from "@/components/ui/sidebar";
+import { sidebarItems } from "@/constants/Navigation";
 
 const data = {
   user: {
-    name: 'shadcn',
-    email: 'm@example.com',
-    avatar: '/avatars/shadcn.jpg',
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
   },
-}
-
-// Menu items.
-const items = [
-    {
-        title: "Home",
-        url: "#",
-        icon: Home,
-    },
-    {
-        title: "Inbox",
-        url: "#",
-        icon: Inbox,
-    },
-    {
-        title: "Calendar",
-        url: "#",
-        icon: Calendar,
-    },
-    {
-        title: "Search",
-        url: "#",
-        icon: Search,
-    },
-    {
-        title: "Settings",
-        url: "#",
-        icon: Settings,
-    },
-];
+};
 </script>
---
+++ constants/Navigation.ts
@@ -0,0 +1,22 @@
+export const sidebarItems = [
+  {
+    name: 'Tableau de bord',
+    path: '/home',
+    icon: 'Dashboard'
+  },
+  {
+    name: 'Notification',
+    path: '/#',
+    icon: 'Sonner'
+  },
+  {
+    name: 'Réalisations',
+    path: '/#',
+    icon: 'Graph'
+  },
+  {
+    name: 'Historique',
+    path: '/#',
+    icon: 'Time'
+  },
+]
