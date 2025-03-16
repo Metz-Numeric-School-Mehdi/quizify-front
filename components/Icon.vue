e02398fa05af3a88bfc2d653bb96912115aa9331
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-03-16 20:25:29 +0100

    ✨💄 feat(ui): add component for icon

---
+++ components/Icon.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref, watchEffect } from "vue";
+import * as icons from "lucide-vue-next";
+import type IconProps from "~/types/components/icon/IconProps";
+
+const { name, size, color, strokeWidth, defaultClass } = defineProps<IconProps>();
+
+const icon = ref<Component | null>(null);
+
+watchEffect(() => {
+  icon.value = icons[name as keyof typeof icons] as Component;
+});
+</script>
+
+<template>
+  <component
+    :is="icon"
+    :size="size"
+    :color="color"
+    :stroke-width="strokeWidth"
+    :default-class="defaultClass"
+  />
+</template>
