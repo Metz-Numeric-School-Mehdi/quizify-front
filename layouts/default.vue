f54cc1e622f8971b00278f7c0ff42d1d865dd7a0
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-04-06 21:05:51 +0200

    🐛 fix: default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,13 @@
-<script setup lang="ts">
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import AppSidebar from "@/components/AppSidebar.vue";
-</script>
-
 <template>
-  <SidebarProvider>
-    <AppSidebar />
-    <main>
-      <SidebarTrigger />
-      <slot />
-    </main>
-  </SidebarProvider>
+  <div class="flex items-center justify-between gap-2 p-5">
+    <img class="w-16" src="../assets/img/icon.png" alt="Icon quizify" />
+    <div class="flex items-center gap-2">
+      <ThemeToggle />
+      <HomeUserNav />
+    </div>
+  </div>
+  <DropdownMenuSeparator class="w-full" />
+  <slot />
 </template>
+
+<script setup lang="ts"></script>
