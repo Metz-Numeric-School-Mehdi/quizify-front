0de490fc1b2e5ad49efe0616d255a80a10a29964
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-04-29 08:11:04 +0200

    🐛 fix(tailwind): add default border radius value to tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,7 +32,8 @@ export default {
 			borderRadius: {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)'
+				sm: 'calc(var(--radius) - 4px)',
+				default: '30px'
 			},
 			colors: {
 				"primary-linear": 'hsl(var(--primary-linear))',
