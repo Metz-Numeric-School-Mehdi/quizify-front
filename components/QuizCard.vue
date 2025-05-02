2a097f99e542b427ee76f13b2a49b6bb7ceea080
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-05-02 08:07:21 +0200

    🐛💄 fix(ui): update QuizCard component to use default border radius for consistency

--- components/QuizCard.vue
+++ components/QuizCard.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="relative rounded-xl cursor-pointer overflow-hidden w-80 h-80 transform transition-transform group">
+  <div class="relative rounded-default cursor-pointer overflow-hidden w-80 h-80 transform transition-transform group">
     <div class="absolute inset-0 group-hover:scale-105 transition-transform duration-300">
       <img class="w-full h-full opacity-70 group-hover:opacity-100 object-cover" src="@/assets/img/card_wallp.jpeg"
         alt="Quiz Image" />
     </div>
     <div
-      class="absolute bottom-0 left-0 right-0 z-10 p-6 text-center bg-white bg-opacity-30 backdrop-blur-lg rounded-xl m-4">
+      class="absolute bottom-0 left-0 right-0 z-10 p-6 text-center bg-white bg-opacity-30 backdrop-blur-lg rounded-default m-4">
       <h2 class="text-2xl font-bold text-white drop-shadow-lg">{{ quizTitle }}</h2>
       <div
-        class="mt-4 inline-block bg-gradient-to-r from-pink-500 to-yellow-500 text-white py-1 px-3 rounded-full text-sm shadow-lg">
+        class="mt-4 inline-block bg-gradient-to-r from-pink-500 to-yellow-500 text-white py-1 px-3 rounded-default text-sm shadow-lg">
         {{ quizTime }}
       </div>
     </div>
