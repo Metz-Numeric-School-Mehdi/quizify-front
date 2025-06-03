6be76b5fd638c60a8741d4caa5e89204e241a18b
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-06-03 14:26:42 +0200

    ✨ feat: update Home component to use Icon for button and fix quiz state references

--- components/home/Home.vue
+++ components/home/Home.vue
@@ -9,31 +9,32 @@
     <div class="flex justify-end mb-4 z-10">
       <button @click="useQuiz.state.openModal = true"
         class="flex items-center gap-2 px-4 py-2 sm:px-6 sm:py-3 bg-gradient-to-r from-pink-400 via-purple-400 to-blue-400 text-white font-bold rounded-full shadow-lg hover:scale-105 hover:shadow-2xl transition-all duration-300 animate-bounce-slow text-base sm:text-lg">
-        <span class="text-xl sm:text-2xl">➕</span>
+        <span class="text-xl sm:text-2xl">
+          <Icon name="Plus" :stroke-width="2.5" :size="20" />
+        </span>
         <span class="hidden sm:inline">Créer un quiz</span>
         <span class="inline sm:hidden">Créer</span>
       </button>
     </div>
     <CreateQuizModal @close="useQuiz.state.openModal = false" />
-    <div v-if="!useQuiz.state.ready && !useQuiz.state.allQuiz" class="w-full h-screen flex justify-center items-center">
+    <div v-if="!useQuiz.state.ready && !useQuiz.state.quizzes" class="w-full h-screen flex justify-center items-center">
       <p class="text-center text-2xl">Loading...</p>
     </div>
-    <main v-else
-      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 sm:gap-6 p-2 sm:p-6 bg-white/80 rounded-xl shadow-xl">
-      <QuizCard v-for="quiz in useQuiz.state.allQuiz" :key="quiz.id" :title="quiz.title" :duration="quiz.duration" />
+    <main v-else class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 sm:gap-6 p-2 sm:p-6">
+      <QuizCard v-for="quiz in useQuiz.state.quizzes" :key="quiz.id" :img="quiz.thumbnail_url || ''" :title="quiz.title" :duration="quiz.duration" />
     </main>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { quizStore } from '~/stores/quizStore';
+import { useQuizStore } from '~/stores/quizStore'
 import QuizCard from '../QuizCard.vue';
 import CreateQuizModal from '../modals/quiz/CreateQuizModal.vue';
 
-const useQuiz = quizStore()
+const useQuiz = useQuizStore();
 
 onMounted(async () => {
   await nextTick()
-  useQuiz.fetch();
+  useQuiz.getAll();
 });
 </script>
