79434a3519644c3960796de87e59474d1a8c91d1
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-06-03 14:26:01 +0200

    ✨ feat: add dynamic image support to QuizCard component

--- components/QuizCard.vue
+++ components/QuizCard.vue
@@ -1,7 +1,9 @@
 <template>
   <div
     class="relative rounded-xl cursor-pointer overflow-hidden w-full max-w-xs h-64 sm:h-72 mx-auto bg-white shadow-md border border-gray-100 group transition-all duration-200 hover:shadow-xl hover:scale-105">
-    <img class="w-full h-32 sm:h-36 object-cover" src="@/assets/img/card_wallp.jpeg" alt="Quiz Image" />
+    <img v-if="img" class="w-full h-32 sm:h-36 object-cover" :src="img" alt="Quiz Image" />
+    <img v-else class="w-full h-32 sm:h-36 object-cover" src="@/assets/img/card_wallp.jpeg" alt="Quiz Image" />
+
     <div class="flex flex-col items-center justify-center px-4 py-4 h-32 sm:h-36">
       <h2 class="text-lg sm:text-xl font-bold text-gray-800 mb-1 truncate w-full text-center">
         {{ title }}
@@ -9,14 +11,10 @@
       <div class="text-xs sm:text-sm text-gray-500 mb-2 truncate w-full text-center">
         {{ duration }} min
       </div>
-      <button
-        class="mt-2 px-4 py-2 bg-pink-500 text-white rounded-full font-semibold shadow transition hover:bg-pink-600">
-        Jouer
-      </button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{ title: string, duration: number }>()
+const { title, duration, img } = defineProps<{ title: string, duration: number, img: string }>()
 </script>
