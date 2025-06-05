8611111128d30b70f6576060778499f0883423a9
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-06-05 09:21:59 +0200

    ✨ feat: add QuizInfoEditor and QuizInfoFields components for quiz information management

---
+++ components/quiz/QuizInfoEditor.vue
@@ -0,0 +1,25 @@
+<template>
+    <QuizInfoFields ref="fieldsRef" />
+    <div class="flex justify-end">
+        <Button class="bg-pink-500 text-white font-bold p-2 mt-4 rounded-xl hover:bg-pink-600 transition" type="button"
+            @click="update">
+            Enregistrer les modifications
+        </Button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import QuizInfoFields from './QuizInfoFields.vue'
+import { useQuizStore } from '~/stores/quizStore'
+const useQuiz = useQuizStore()
+
+const fieldsRef = ref<any>(null)
+
+const update = async () => {
+    if (useQuiz.state.quiz && fieldsRef.value) {
+        Object.assign(useQuiz.state.quizForm, fieldsRef.value.formLocal)
+        await useQuiz.update(useQuiz.state.quiz.id, useQuiz.state.quizForm)
+    }
+}
+</script>
