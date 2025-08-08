decd8d10b0798f9fe88ca56446c4b475511303d9
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-08-08 09:06:36 +0200

    ✨ feat: add QuizCategory, QuizLevel, Quiz, and QuizMeta interfaces to define quiz structure and metadata

--- types/quiz/Quiz.d.ts
+++ types/quiz/Quiz.d.ts
@@ -1,3 +1,42 @@
+export interface QuizCategory {
+  id: number;
+  name: string;
+  created_at: string;
+}
+
+export interface QuizLevel {
+  id: number;
+  name: string;
+  created_at: string;
+  updated_at: string | null;
+  deleted_at: string | null;
+}
+
+export interface Quiz {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  is_public: number;
+  level: QuizLevel;
+  user_id: number;
+  duration: number | null;
+  pass_score: number | null;
+  thumbnail: string | null;
+  thumbnail_url: string | null;
+  tags: any[];
+  category: QuizCategory;
+  created_at: string;
+  updated_at: string;
+  status: string;
+}
+
+export interface QuizMeta {
+  total: number;
+  per_page: number;
+  current_page: number;
+  last_page: number;
+}
 import type { Question } from "../question/Question";
 import type { Tag } from "../tag/Tag";
 import type { Category } from "./Category";
