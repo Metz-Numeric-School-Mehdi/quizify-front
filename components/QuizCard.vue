cc7080d9993487af5cb740155d5262a079000121
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-08-27 07:54:06 +0200

    🚩 feat: update quiz duration display to show 'Temps infini' for zero duration

--- components/QuizCard.vue
+++ components/QuizCard.vue
@@ -41,7 +41,7 @@
               <circle cx="12" cy="12" r="10" />
               <polyline points="12 6 12 12 16 14" />
             </svg>
-            {{ duration ?? '?' }} min
+            {{ duration === 0 ? 'Temps infini' : duration + ' min' }}
           </span>
         </div>
         
